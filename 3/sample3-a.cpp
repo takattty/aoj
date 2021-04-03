fdbf36e1c78ee585333613c4f9dd50b44b948f77
takattty <r9r_celtics_t9t@icloud.com>
2021-04-03 23:28:12 +0900

    done 3-a

---
+++ 3/sample3-a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i=0; i<1000; i++) {
+    cout << "Hello World" << endl;
+  }
+}
+
+//参考コード
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+// 	for(int i=0; i<1000; ++i) cout << "Hello World" << endl; //こういう風に後ろに書けるんですね。
+// }
