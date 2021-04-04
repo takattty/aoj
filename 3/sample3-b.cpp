50f3ec2fc4235f06eb5e1d67538716568ce01b6a
takattty <r9r_celtics_t9t@icloud.com>
2021-04-04 23:56:21 +0900

    learn 3-b

--- 3/sample3-b.cpp
+++ 3/sample3-b.cpp
@@ -14,3 +14,18 @@ int main() {
     }
   }
 }
+
+//参考コード
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+// 	int i=1,x;
+// 	cin >> x;
+// 	while(x!=0){
+// 		cout << "Case " << i <<": " << x << endl;
+// 		i++;
+// 		cin >> x;//ここで一つづつ代入している。
+// 	}
+// 	return 0;
+// }
