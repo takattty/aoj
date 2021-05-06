4d6e69140d239b08b5543531bbdd0c4c637ea998
takattty <r9r_celtics_t9t@icloud.com>
2021-05-06 17:08:45 +0900

    done 3-c

--- 3/sample3-c.cpp
+++ 3/sample3-c.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main() {
   int a, b;
-  cin >> a >> b;
-  cout << a << b << endl;
+
+  while(1) {
+    cin >> a >> b;
+    if(a == 0 && b == 0) break;
+    if(a > b) {
+      cout << b << " " << a << endl;
+    } else {
+      cout << a << " " << b << endl;
+    }
+  }
 }
