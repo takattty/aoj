97ba8aa9ffca44ea528a64182fe3122278c4c0cd
takattty <r9r_celtics_t9t@icloud.com>
2021-05-07 11:31:09 +0900

    done 5-c

---
+++ 5/5-c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  while(1) {
+    if(a == 0 && b == 0) break;
+    cin >> a >> b;
+    for(int i=0; i<a; i++) {
+      for(int j=0; j<b; j++) {
+        if((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+          cout << "#";
+        } else {
+          cout << ".";
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+}
