d819b8be17db1efd20baf073eb1ce2f2fbf6ad4f
takattty <r9r_celtics_t9t@icloud.com>
2021-05-19 11:17:32 +0900

    done 6-c

--- 6/6-c.cpp
+++ 6/6-c.cpp
@@ -5,35 +5,27 @@ int main() {
   int n, b, f, r, v;
   cin >> n;
 
-  vector<vector<int>> first(3, vector<int>(10));
-  vector<vector<int>> second(3, vector<int>(10));
-  vector<vector<int>> third(3, vector<int>(10));
-  vector<vector<int>> forth(3, vector<int>(10));
+  vector<vector<int> > first(3, vector<int>(10));
+  vector<vector<int> > second(3, vector<int>(10));
+  vector<vector<int> > third(3, vector<int>(10));
+  vector<vector<int> > forth(3, vector<int>(10));
 
   for(int i=0; i<n; i++) {
     cin >> b >> f >> r >> v;
     if(b == 1) {
-      first.at(f-1).at(r-1) = v;
+      first.at(f-1).at(r-1) += v;
     } else if(b == 2) {
-      second.at(f-1).at(r-1) = v;
+      second.at(f-1).at(r-1) += v;
     } else if(b == 3) {
-      third.at(f-1).at(r-1) = v;
+      third.at(f-1).at(r-1) += v;
     } else {
-      forth.at(f-1).at(r-1) = v;
+      forth.at(f-1).at(r-1) += v;
     }
   }
 
   for(int i=0; i<3; i++) {
     for(int j=0; j<10; j++) {
-      cout << first.at(i).at(j);
-    }
-    cout << endl;
-  }
-  cout << "####################" << endl;
-
-  for(int i=0; i<3; i++) {
-    for(int j=0; j<10; j++) {
-      cout << second.at(i).at(j);
+      cout << " " << first.at(i).at(j);
     }
     cout << endl;
   }
@@ -41,7 +33,7 @@ int main() {
 
   for(int i=0; i<3; i++) {
     for(int j=0; j<10; j++) {
-      cout << third.at(i).at(j);
+      cout << " " << second.at(i).at(j);
     }
     cout << endl;
   }
@@ -49,7 +41,15 @@ int main() {
 
   for(int i=0; i<3; i++) {
     for(int j=0; j<10; j++) {
-      cout << forth.at(i).at(j);
+      cout << " " << third.at(i).at(j);
+    }
+    cout << endl;
+  }
+  cout << "####################" << endl;
+
+  for(int i=0; i<3; i++) {
+    for(int j=0; j<10; j++) {
+      cout << " " << forth.at(i).at(j);
     }
     cout << endl;
   }
