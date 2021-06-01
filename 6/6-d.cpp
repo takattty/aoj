eaf597c3ec2e16ce300111cd975bd33ad1666aec
takattty <r9r_celtics_t9t@icloud.com>
2021-06-01 22:36:01 +0900

    done 7-a

---
+++ 6/6-d.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int> > a(n, vector<int>(10));
+  vector<int> b;
+
+  for(int i=0; i<n; i++) {
+
+  }
+  for(int i=0; i<m; i++) {
+
+  }
+}
