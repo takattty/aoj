758bf37b9c9e2664a78c7ea9b51b5fdefa67e550
takattty <r9r_celtics_t9t@icloud.com>
2021-08-18 23:50:37 +0900

    learn for_each

--- 2/sample2-c.cpp
+++ 2/sample2-c.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -8,9 +7,17 @@ int main() {
   cin >> a >> b >> c;
 
   int x[3];
+  // vector<int> y(3);
   x[0] = a;
   x[1] = b;
   x[2] = c;
+  // y[0] = a;
+  // y[1] = b;
+  // y[2] = c;
+
+  // for_each(y.begin(), y.end(), [](int z) {
+  //   cout << z << endl;
+  // });
 
   sort(x, x+3);
   cout << x[0] << " " << x[1] << " " << x[2] << endl;
