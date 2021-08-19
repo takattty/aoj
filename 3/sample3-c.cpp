1fd01d204145936903cfa6b93c7b9052ad4fe0e3
takattty <r9r_celtics_t9t@icloud.com>
2021-08-19 12:04:36 +0900

    modify 3-c

--- 3/sample3-c.cpp
+++ 3/sample3-c.cpp
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int main() {
+//   int a, b;
+
+//   while(1) {
+//     cin >> a >> b;
+//     if(a == 0 && b == 0) break;
+//     if(a > b) {
+//       cout << b << " " << a << endl;
+//     } else {
+//       cout << a << " " << b << endl;
+//     }
+//   }
+// }
+
 int main() {
   int a, b;
-
-  while(1) {
-    cin >> a >> b;
-    if(a == 0 && b == 0) break;
+  cin >> a >> b;
+  while(a != 0 && b != 0) {
     if(a > b) {
       cout << b << " " << a << endl;
     } else {
-      cout << a << " " << b << endl;
+      cout << a << " " << b << endl; 
     }
+    cin >> a >> b;
   }
+  return 0;
 }
