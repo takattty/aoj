dbf17491d22228718356320a0ddb100e1a4b194f
takattty <r9r_celtics_t9t@icloud.com>
2021-08-19 11:43:58 +0900

    modify 3-b

--- 3/sample3-b.cpp
+++ 3/sample3-b.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int a, i=0;
-  vector<int> num(10000);
-  while(cin>>a) {
-    if(a==0) {
-      return 0;
-    } else {
-      num[i] = a;
-      cout << "Case" << " " << i+1 << ":" << " " << num[i] << endl;
-      i++;
-    }
-  }
-}
+// int main() {
+//   int a, i=0;
+//   vector<int> num(10000); // 今回は入力値を覚える必要が無いから、この配列は絶対要らない。
+//   while(cin>>a) {
+//     if(a==0) {
+//       return 0;
+//     } else {
+//       num[i] = a;
+//       cout << "Case" << " " << i+1 << ":" << " " << num[i] << endl;
+//       i++;
+//     }
+//   }
+// }
 
 //参考コード
 // #include <iostream>
@@ -29,3 +29,15 @@ int main() {
 // 	}
 // 	return 0;
 // }
+
+int main() {
+  int i=1, x;
+  cin >> x;
+  while(x != 0) {
+    cout << "Case " << i << ": " << x << endl;
+    i++;
+    // ここで大事な事は、余計な値を記憶しない事。大事なのは、iの値が記憶され続ける事。
+    cin >> x;
+  }
+  return 0;
+}
